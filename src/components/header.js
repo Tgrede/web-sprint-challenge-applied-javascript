cddb2c33cb96721582dc0e344fdf1e9067096771
Trenten grede <Trenten.grede@gmail.com>
2021-01-15 12:53:25 -0600

    comment changes

--- src/components/header.js
+++ src/components/header.js
@@ -12,23 +12,29 @@ const Header = (title, date, temp) => {
   //  </div>
   //
 
+
+  //creates all needed elements
   const header = document.createElement('div')
   const dateSpan = document.createElement('span')
   const titleH1 = document.createElement('h1')
   const tempSpan = document.createElement('span')
 
+  //adds classes to all elements
   header.classList.add('header')
   dateSpan.classList.add('date')
   tempSpan.classList.add('temp')
 
+  //updates textcontent of elements
   dateSpan.textContent = date
   titleH1.textContent = title
   tempSpan.textContent = temp
 
+  //appends elements to header
   header.appendChild(dateSpan)
   header.appendChild(titleH1)
   header.appendChild(tempSpan)
 
+  //returns just the header that everything is appended to already
   return header
 }
 
@@ -39,8 +45,9 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
-  const newHeader = Header('Test Title', '01/15/2021', '33°')
-  document.querySelector(selector).appendChild(newHeader)
+
+  const newHeader = Header('Test Title', '01/15/2021', '33°') //creates new test header
+  document.querySelector(selector).appendChild(newHeader) //appends test header to the DOM on element selector
 }
 
 export { Header, headerAppender }
